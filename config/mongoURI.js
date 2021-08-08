3e10b3640aa2eebbcd935762e0ea66b323d55904
Irit Kushwaha <iritkushwaha2607@gmail.com>
2021-08-08 12:38:44 +0530

    front

--- config/mongoURI.js
+++
@@ -1,5 +0,0 @@
-const dotenv = require("dotenv");
-
-dotenv.config();
-
-module.exports = `mongodb+srv://admin:${process.env.PASSWORD}@subscriptz.2ozq8.mongodb.net/Koderz?retryWrites=true&w=majority`;
